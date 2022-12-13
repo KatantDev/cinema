500639bb20f55567e9e6187ea935b764687003ef
KatantDev <vlad26@mail.ru>
2022-12-14 00:16:30 +1000

    Added structs and serialize for films (using circle linked lists)

--- main.c
+++ main.c
@@ -1,38 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-// Считываем данные о фильмах из файла.
-void get_films_from_file(const char *filename) {
-    FILE *films_txt = fopen(filename, "r");
-    if (films_txt == NULL) {
-        printf("Ошибка: отсутствует файл с фильмами.\n");
-        return;
-    }
-
+typedef struct Film {
     char title[96];
     int year;
     char countries[64];
     char genres[64];
     float rating;
 
+    struct Film *prev;
+    struct Film *next;
+} Film;
+
+typedef struct Films {
+    int size;
+    Film *current;
+} Films;
+
+// Создание кольцевого списка
+Films *create_films_ring() {
+    Films *films = (Films*)malloc(sizeof(Films));
+    films->size = 0;
+    films->current = NULL;
+    return films;
+}
+
+// Добавление нового фильма в список
+Film* add_film(Films *ring) {
+    Film *previous = NULL;
+    Film *film = (Film*)malloc(sizeof(Film));
+
+    if (ring->current == NULL) {
+        ring->current = film;
+        film->next = film->prev = film;
+    } else {
+        previous = ring->current->next->prev;
+        film->next = ring->current->next;
+        film->prev = ring->current;
+        previous->prev = film;
+
+        ring->current->next = film;
+        ring->current = film;
+    }
+    ring->size++;
+    return film;
+}
+
+// Считываем данные о фильмах из файла
+Films* get_films_from_file(const char *filename) {
+    FILE *films_txt = fopen(filename, "r");
+    if (films_txt == NULL) {
+        printf("Ошибка: отсутствует файл с фильмами.\n");
+        return NULL;
+    }
+
+    Films* films = create_films_ring();
     while (!feof(films_txt)) {
-        fgets(title, sizeof(title), films_txt);
-        fscanf(films_txt, "%d\n", &year);
-        fgets(countries, sizeof(countries), films_txt);
-        fgets(genres, sizeof(genres), films_txt);
-        fscanf(films_txt, "%f\n", &rating);
+        Film *film = add_film(films);
+        fgets(film->title, sizeof(film->title), films_txt);
+        fscanf(films_txt, "%d\n", &film->year);
+        fgets(film->countries, sizeof(film->countries), films_txt);
+        fgets(film->genres, sizeof(film->genres), films_txt);
+        fscanf(films_txt, "%f\n", &film->rating);
 
-        strtok(title, "\n");
-        strtok(countries, "\n");
-        strtok(genres, "\n");
-
-
-        printf("%s\n%d\n%s\n%s\n%.1f\n\n", title, year, countries, genres, rating);
+        strtok(film->title, "\n");
+        strtok(film->countries, "\n");
+        strtok(film->genres, "\n");
     }
     fclose(films_txt);
+    return films;
+}
+
+// Вывод полного двусвязного списка для наглядности того, что все действительно работает
+void print_circle(Films* films) {
+    Film *film = films->current;
+    do {
+        printf("%s\n%d\n%s\n%s\n%.1f\n-------\n", film->title, film->year, film->countries, film->genres, film->rating);
+        film = film->next;
+    }
+    while (films->current != film);
 }
 
 int main() {
-    get_films_from_file("./films.txt");
+    Films *films = get_films_from_file("./films.txt");
+    print_circle(films);
     return 0;
 }
