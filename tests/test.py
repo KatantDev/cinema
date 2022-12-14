3c82aa002b5d2aca525d00eda3b2fbd9f0aa61cc
KatantDev <vlad26@mail.ru>
2022-12-14 14:35:05 +1000

    Removed unused prints

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,6 @@ import numpy as np
 proc = subprocess.Popen("./cinema", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 (out_string, err_string) = proc.communicate()
 
-print(os.listdir('../'))
 # Проверяем вывод ошибку, если файл пуст
 if os.stat('../films.txt').st_size == 0 and 'Ошибка: файл пуст.' in out_string.decode('utf-8').strip():
     print('Test passed! File is empty.')
@@ -16,8 +15,6 @@ if os.stat('../films.txt').st_size == 0 and 'Ошибка: файл пуст.' i
 out = [s.decode("utf-8") for s in out_string.split(b"\n") if (len(s) > 0)]
 with open('../films.txt', 'r') as file:
     data = file.read().split('\n')
-    print(data)
-    print(out[:len(data)])
     result = np.in1d(data, out[:len(data)])
     if False in result:
         print('Test failed! Data in stdout of C have difference with data from file.')
