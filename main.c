7d8c7cecc30e290f7d1a2d4e228384554dc24ba0
curlyxx <120436160+Tryakshin@users.noreply.github.com>
2022-12-14 09:45:36 +0000

    Released functional for serialize and printing films.txt file with tests

--- main.c
+++ main.c
@@ -55,6 +55,14 @@ Films* get_films_from_file(const char *filename) {
         return NULL;
     }
 
+    fseek(films_txt, 0, SEEK_END);
+    long size = ftell(films_txt);
+    if (0 == size) {
+        printf("Ошибка: файл пуст.\n");
+        return NULL;
+    }
+    fseek(films_txt, 0, SEEK_SET);
+
     Films* films = create_films_ring();
     while (!feof(films_txt)) {
         Film *film = add_film(films);
@@ -76,14 +84,16 @@ Films* get_films_from_file(const char *filename) {
 void print_circle(Films* films) {
     Film *film = films->current;
     do {
-        printf("%s\n%d\n%s\n%s\n%.1f\n-------\n", film->title, film->year, film->countries, film->genres, film->rating);
+        printf("%s\n%d\n%s\n%s\n%.1f\n", film->title, film->year, film->countries, film->genres, film->rating);
         film = film->next;
     }
     while (films->current != film);
 }
 
 int main() {
-    Films *films = get_films_from_file("./films.txt");
-    print_circle(films);
+    Films *films = get_films_from_file("../films.txt");
+    if (films != NULL) {
+        print_circle(films);
+    }
     return 0;
 }
